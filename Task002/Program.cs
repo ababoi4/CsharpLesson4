939e41b15215cd83817de2041662a886323923c0
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-19 15:20:20 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int D = number % 10;
+        number = number / 10;
+        sum = sum + D;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число ->");
+System.Console.WriteLine($"Сумма цифр в данном числе равна {SumNumber(number)}");
